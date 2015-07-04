cef7642e4274f446c89be3b1fc187bda7626364b
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-07-04 16:57:48 -0700

    Initial project setup.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to infinite-raid
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/infinite_raid/core_test.clj
@@ -0,0 +1,7 @@
+(ns infinite-raid.core-test
+  (:require [clojure.test :refer :all]
+            [infinite-raid.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
